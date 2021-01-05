af5906bafa0c99acde0966421cdef59233934d86
nikitha <gullapalli.nikitha@gmail.com>
2021-01-04 22:16:45 -0800

    social

---
+++ app/src/main/java/com/example/social/SocialAppWidget.java
@@ -0,0 +1,45 @@
+package com.example.social;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class SocialAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,int appWidgetId) {
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.social_app_widget);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+        views.setImageViewResource(R.id.appwidget_text,R.drawable.socail);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
