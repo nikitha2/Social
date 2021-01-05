98b39de26a5cc3be5dddb158f18f44bff96cc09c
nikitha <gullapalli.nikitha@gmail.com>
2021-01-04 21:52:31 -0800

    social

--- app/src/main/java/com/example/social/SocialAppWidgetProvider.java
+++ app/src/main/java/com/example/social/SocialAppWidgetProvider.java
@@ -1,12 +1,38 @@
 package com.example.social;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 
-public class SocialAppWidgetProvider extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
+public class SocialAppWidgetProvider extends AppWidgetProvider {
 
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            updateAppWidget(context, appWidgetManager, appWidgetId, intent);
+        }
+    }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Intent intent) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Get the layout for the App Widget and attach an on-click listener
+        // to the button
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.social_appwidget_layout);
+        views.setOnClickPendingIntent(R.id.widgetGrid, pendingIntent);
+        views.setImageViewResource(R.id.widgetGrid,R.drawable.socail);
+
+        // Tell the AppWidgetManager to perform an update on the current app widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
